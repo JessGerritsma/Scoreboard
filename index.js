accc5339adae77148333ea72fdc037e485d3d9c1
JessGerritsma <111094591+JessGerritsma@users.noreply.github.com>
2022-09-13 09:50:18 +1000

    add-files

---
+++ index.js
@@ -0,0 +1,50 @@
+//HOME SCORE SCRIPT
+let homeScoreBtnOne = document.getElementById("home-score-btn-1")
+let homeScoreBtnTwo = document.getElementById("home-score-btn-2")
+let homeScoreBtnThree = document.getElementById("home-score-btn-3")
+let homeStoreEl =document.getElementById("home-score")
+let homeScore = 0
+
+function increaseHomeScoreOne(){
+    homeScore += 1
+    homeStoreEl.textContent = homeScore
+}
+function increaseHomeScoreTwo(){
+    homeScore += 2
+    homeStoreEl.textContent = homeScore
+}
+function increaseHomeScoreThree(){
+    homeScore += 3
+    homeStoreEl.textContent = homeScore
+}
+
+//GUEST SCORE SCRIPT
+let guestScoreBtnOne = document.getElementById("guest-score-btn-1")
+let guestScoreBtnTwo = document.getElementById("guest-score-btn-2")
+let guestScoreBtnThree = document.getElementById("guest-score-btn-3")
+let guestStoreEl =document.getElementById("guest-score")
+let guestScore = 0
+
+function increaseGuestScoreOne(){
+    guestScore += 1
+    guestStoreEl.textContent = guestScore
+}
+function increaseGuestScoreTwo(){
+    guestScore += 2
+    guestStoreEl.textContent = guestScore
+}
+function increaseGuestScoreThree(){
+    guestScore += 3
+    guestStoreEl.textContent = guestScore
+}
+
+//RESET BUTTON SCRIPT
+
+let resetGame = document.getElementById("reset-game-btn")
+
+function resetScore(){
+    homeScore = 0
+    homeStoreEl.textContent = homeScore
+    guestScore = 0
+    guestStoreEl.textContent = guestScore
+}
